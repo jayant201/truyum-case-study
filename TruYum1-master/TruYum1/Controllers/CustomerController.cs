9d560b225449ab39185755356219098c4912a422
jayant201 <78479485+jayant201@users.noreply.github.com>
2021-03-22 11:50:37 +0530

    Add files via upload

---
+++ TruYum1-master/TruYum1/Controllers/CustomerController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TruYum1.Models;
+
+namespace TruYum1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        TruYumContext db = new TruYumContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var list = db.MenuItems.ToList();
+            ViewBag.Clist = db.Categories.ToList();
+            return View(list);
+        }
+        public ActionResult Cart(int? id)
+        {
+            if (id != null)
+            {
+                Cart cart = new Cart()
+                {
+                    MenuItemId = (int)id
+                };
+                db.Carts.Add(cart);
+                db.SaveChanges();
+                return RedirectToAction("ViewCart");
+            }
+            else
+            {
+                return View("Cart");
+            }
+        }
+        public class CartList
+        {
+            public int CartId { get; set; }
+            public string Name { get; set; }
+            public bool FreeDelivery { get; set; }
+            public double Price { get; set; }
+        }
+        public ActionResult ViewCart()
+        {
+            var list = (from cart in db.Carts
+                        join menu in db.MenuItems on cart.MenuItemId equals menu.Id
+                        orderby cart.Id
+                        select new CartList()
+                        {
+                            CartId = cart.Id,
+                            Name = menu.Name,
+                            FreeDelivery = menu.FreeDelivery,
+                            Price = menu.Price,
+                        }).ToList();
+
+            return View(list);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                Cart Cid = db.Carts.Find(id);
+                db.Carts.Remove(Cid);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ TruYum1-master/TruYum1/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace TruYum1.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public int MenuItemId { get; set; }
+        public MenuItem MenuItem { get; set; }
+    }
+}
